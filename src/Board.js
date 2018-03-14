d9f4a9f7b33083089399bf5f9fba97571580fe32
brayheart <broncobray77@gmail.com>
2018-03-14 12:07:37 -0700

    Finished column & rows

--- src/Board.js
+++ src/Board.js
@@ -79,11 +79,31 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      var count = 0;
+      for(var i = 0; i < this.attributes[rowIndex].length; i ++){
+        if(this.attributes[rowIndex][i] === 1){
+          count++;
+        };
+      };
+
+      if(count > 1){
+        return true;
+      }else{
+        return false; // fixme
+      };
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
+      var nRows = this.attributes.n;
+      var board = this;
+      //console.log(this);
+      for(var i = 0; i < nRows; i ++){
+        if(board.hasRowConflictAt(i)){
+          return true;
+        }
+      };
+
       return false; // fixme
     },
 
@@ -94,11 +114,32 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      var count = 0;
+
+      for(var i = 0; i < this.attributes.n; i++){
+        if(this.attributes[i][colIndex] === 1){
+          count++;
+        };
+      };//for every row at colIndex
+
+      if(count > 1){
+        return true;
+      }else{
+        return false; // fixme
+      };
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
+      var board = this;
+      var nColumn = this.attributes.n;
+
+      for(var i = 0; i < nColumn; i ++){
+        if(board.hasColConflictAt(i)){
+          return true;
+        };
+      };
+      
       return false; // fixme
     },
 
@@ -109,11 +150,30 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
+      console.log(majorDiagonalColumnIndexAtFirstRow);
+
+
       return false; // fixme
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
+      // i === row && j === column
+      for(var i = 0; i < this.attributes.n; i++){
+        if(this.attributes[i].includes(1)){
+          for(var j = 0; j < this.attributes.n; j++){
+            if(this.attributes[i][j] === 1){
+              console.log("row", i);
+              console.log("column", j);
+              if(this.hasMajorDiagonalConflictAt([i,j])){
+                return true;
+              };
+            };
+          };// find 1's in the array
+        };
+      };// find the first row that contains a 1
+
+
       return false; // fixme
     },
 
